e02427df989f983efff8b514193820bfbd2ad382
vzaveri12 <vaibhavzaveri114@gmail.com>
2022-01-27 08:13:53 -0800

    comments on tests

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -25,11 +25,10 @@ class BankAccountTest {
     @Test
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""));         // empty string
-
-        assertFalse(BankAccount.isEmailValid("ab@")); // This email address was expected to be false, but was defined true
-        assertFalse(BankAccount.isEmailValid("@gmail.com")); //This email address has no username
-        assertTrue(BankAccount.isEmailValid("abc_abc@gmail.com")); // A possible email address, its complete
+        assertFalse( BankAccount.isEmailValid("")); // empty string, invalid, cannot be an email as it doesnt have a username & domainname, border case
+        assertFalse(BankAccount.isEmailValid("ab@")); // This email address doesnt have domain name so its a border case
+        assertFalse(BankAccount.isEmailValid("@gmail.com")); //This email address has no username, so its a border case
+        assertTrue(BankAccount.isEmailValid("abc_abc@gmail.com")); // A possible email address, its complete so its not a boder case.
         
     }
 
@@ -44,3 +43,36 @@ class BankAccountTest {
     }
 
 }
+
+//comments for Equivalence partition
+// 1) a@b.com -> valid
+//    a_a@b.com -> valid
+//    @@b.com -> invalid
+//    .@b.com -> invalid
+//   12@b.com -> valid
+
+// 2) "" -> invalid
+//    a@b.com -> valid
+// a@b.edu -> valid
+
+// 3) "ab@" -> invalid
+//  ab@a -> invalid
+// (0-9)@ ->invalid
+// ab@ab -> invalid
+// @@ ->invalid
+// ab@$ -> invalid
+// ab@a.com -> valid
+
+// 4) "@gmail.com" -> invalid
+//     a@gmail.com -> valid
+//   @@gmail.com ->invalid
+// @gmail.edu ->invalid
+// @yahoo.com  ->invalid
+// ab@gmail.com ->valid
+
+// 5) abc_abc@gmail.com -> valid
+//    abc@gmail.com -> valid
+// $$$@gmail.com -> invalid
+// ab12@gmail.com -> valid
+// 012_abc@gmail.com -> valid
+// @_abc@gmail.com -> invalid
